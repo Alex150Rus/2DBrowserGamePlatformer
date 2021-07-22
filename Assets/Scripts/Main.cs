1e5f6ff1d02993f13ae4cbe280ffa6aca0e895d4
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-07-22 10:33:06 +0300

    Main script has been updated

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,24 +1,33 @@
 using System;
+using PlatformerMVC.Config;
+using PlatformerMVC.Controllers;
+using PlatformerMVC.View;
 using UnityEngine;
 
 namespace PlatformerMVC
 {
-
     public class Main : MonoBehaviour
     {
+        [SerializeField] private SpriteAnimatorConfig _playerConfig;
+        [SerializeField] private int _animationSpeed = 10;
+        [SerializeField] private LevelObjectView _playerView;
+
+        private SpriteAnimatorController _playerAnimator;
+
         private void Awake()
         {
-
+            _playerConfig = Resources.Load<SpriteAnimatorConfig>("PlayerAnimCfg");
+            _playerAnimator = new SpriteAnimatorController(_playerConfig);
+            _playerAnimator.StartAnimation(_playerView._spriteRenderer, AnimState.Run, true, _animationSpeed);
         }
 
         void Update()
         {
-
+            _playerAnimator.Update();
         }
 
         private void FixedUpdate()
         {
-
         }
     }
 }
