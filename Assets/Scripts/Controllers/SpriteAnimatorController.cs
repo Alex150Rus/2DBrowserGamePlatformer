286b3c5023ccb54b02f2b22bb18bec9a494b4f0a
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-07-22 10:21:28 +0300

    SpriteAnimatorController is ready

---
+++ Assets/Scripts/Controllers/SpriteAnimatorController.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using PlatformerMVC.Config;
+using UnityEngine;
+
+namespace PlatformerMVC.Controllers
+{
+    public class SpriteAnimatorController : IDisposable
+    {
+        private sealed class Animation
+        {
+            public AnimState Track;
+            public List<Sprite> Sprites;
+            public bool Loop;
+            public float Speed = 10;
+            public float Counter = 0;
+            public bool Sleep;
+
+            public void Update()
+            {
+                if (Sleep) return;
+                Counter += Time.deltaTime * Speed;
+
+                if (Loop)
+                {
+                    while (Counter > Sprites.Count)
+                    {
+                        Counter -= Sprites.Count;
+                    }
+                }
+                else if (Counter > Sprites.Count)
+                {
+                    Counter = Sprites.Count;
+                    Sleep = true;
+                }
+            }
+        }
+
+        private SpriteAnimatorConfig _config;
+        private Dictionary<SpriteRenderer, Animation> _activeAnimations = new Dictionary<SpriteRenderer, Animation>();
+
+        public SpriteAnimatorController(SpriteAnimatorConfig config)
+        {
+            _config = config;
+        }
+
+        public void StartAnimation(SpriteRenderer spriteRenderer, AnimState track, bool loop, float speed)
+        {
+            if (_activeAnimations.TryGetValue(spriteRenderer, out var animation))
+            {
+                animation.Loop = loop;
+                animation.Speed = speed;
+                animation.Sleep = false;
+                if (animation.Track != track)
+                {
+                    animation.Track = track;
+                    animation.Sprites = _config.Sequences.Find(sequence => sequence.Track == track).Sprites;
+                    animation.Counter = 0;
+                }
+            }
+            else
+            {
+                _activeAnimations.Add(spriteRenderer, new Animation()
+                {
+                    Track = track,
+                    Sprites = _config.Sequences.Find(sequence => sequence.Track == track).Sprites,
+                    Loop = loop,
+                    Speed = speed
+                });
+            }
+        }
+        
+        public void StopAnimation(SpriteRenderer spriteRenderer)
+        {
+            if (_activeAnimations.ContainsKey(spriteRenderer))
+            {
+                _activeAnimations.Remove(spriteRenderer);
+            }
+           
+        }
+
+        public void Update()
+        {
+            foreach (var animation in _activeAnimations)
+            {
+                animation.Value.Update();
+                if (animation.Value.Counter < animation.Value.Sprites.Count)
+                {
+                    animation.Key.sprite = animation.Value.Sprites[(int) animation.Value.Counter];
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _activeAnimations.Clear();
+        }
+    }
+}
