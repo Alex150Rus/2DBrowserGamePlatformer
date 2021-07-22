d05212bab2dd88c7ea27946f0a45bff1a4ccfe2a
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-07-22 09:48:46 +0300

    View has been created

---
+++ Assets/Scripts/View/LevelObjectView.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace PlatformerMVC.View
+{
+    public class LevelObjectView : MonoBehaviour
+    {
+        public Transform _transform;
+        public SpriteRenderer _spriteRenderer;
+        public Collider2D _collider;
+        public Rigidbody2D _rigidbody;
+    }
+}
