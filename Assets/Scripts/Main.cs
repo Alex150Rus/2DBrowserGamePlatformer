a4bf1252435d533dfd15ae48e85cccaac9846bb3
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-07-26 12:37:11 +0300

    cannon has been created. Muzzle is pointing player now

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -11,20 +11,24 @@ namespace PlatformerMVC
         [SerializeField] private SpriteAnimatorConfig _playerConfig;
         [SerializeField] private int _animationSpeed = 15;
         [SerializeField] private LevelObjectView _playerView;
+        [SerializeField] private LevelObjectView _muzzleView;
 
         private SpriteAnimatorController _playerAnimator;
         private PlayerMoveController _playerMoveController;
+        private MuzzleController _muzzleController;
 
         private void Awake()
         {
             _playerConfig = Resources.Load<SpriteAnimatorConfig>("PlayerAnimCfg");
             _playerAnimator = new SpriteAnimatorController(_playerConfig);
             _playerMoveController = new PlayerMoveController(_playerView, _playerAnimator);
+            _muzzleController = new MuzzleController(_muzzleView, _playerView);
         }
 
         void Update()
         {
             _playerMoveController.Update();
+            _muzzleController.Update();
         }
 
         private void FixedUpdate()
